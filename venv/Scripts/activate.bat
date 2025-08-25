64816bf4ff62ead4bebcc74667024c9865c2bba5
On-cloud7 <priyanka.p62003@gmail.com>
2025-08-25 15:15:08 +0530

    files added

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Admin\Desktop\Office-emp-Management\venv
+set VIRTUAL_ENV=C:\Users\Admin\Desktop\Office-emp-Management\office_emp_proj\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
